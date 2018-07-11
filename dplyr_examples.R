4d3ff704e88c97c6ff76147a8a472c4bf59fb127
Michelle R. Jackson <michellejack@umass.edu>
2018-07-11 16:30:40 -0400

    Upload of dplyr examples code.

---
+++ dplyr_examples.R
@@ -0,0 +1,41 @@
+#################################
+#Name: Michelle R. Jackson
+#Original Date: July 11, 2018
+#R version: 3.5.0 "Joy in Playing"
+#Purpose: Code explains how to transform data with the 'dplyr' function
+#Source:http://r4ds.had.co.nz/transform.html#
+#################################
+#Download required packages
+if (!require("dplyr")) install.packages("dplyr")
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("nycflights13")) install.packages("nycflights13")
+#Analyses
+library(nycflights13) #load nycflights13 data set
+library(tidyverse) #load tidyverse dataset
+nycflights13::flights #see flights tibble
+View(flights) #see flights overall
+#dplyr & tibble components 
+#int stands for integers
+#dbl stands for doubles, or real numbers
+#chr stands for character vectors, or strings
+#dttm stands for date-times (a date + a time)
+#lgl stands for logical, vectors that contain only TRUE or FALSE
+#fctr stands for factors, which R uses to represent categorical variables with fixed possible values
+#date stands for dates
+#Pick observations by their values (filter())
+#Reorder the rows (arrange())
+#Pick variables by their names (select())
+#Create new variables with functions of existing variables (mutate())
+#Collapse many values down to a single summary (summarise())
+
+
+
+
+
+
+
+
+
+
+
+
